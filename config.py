6fab685e31ada8ab536cd5e31dcb7054897ccc98
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-06 03:38:01 -0600

    Update and rename config.sample.py to config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 
 class Config(object):
     # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = ""
+    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = ""
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # your domain to show when download file is greater than MAX_FILE_SIZE
-    HTTP_DOMAIN = "https://example.com/"
+    HTTP_DOMAIN = os.environ.get("HTTP_DOMAIN", "https://example.com/")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 14000000000
     # The Telegram API things
-    APP_ID = 12345
-    API_HASH = ""
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # for storing the Telethon session
-    TL_SESSION = "AnyDLBot.session"
-    CHUNK_SIZE = 128
+    TL_SESSION = os.environ.get("TL_SESSION", "AnyDLBot.session")
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
 
